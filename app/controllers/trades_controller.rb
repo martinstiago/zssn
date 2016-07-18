3e33c6607d31e0ee5bb555ecd50a04e905c772a1
Tiago Martins Araujo <tiago.martins.araujo@gmail.com>
2016-07-18 19:14:11 -0300

    refactor trades controller logic to class

--- app/controllers/trades_controller.rb
+++ app/controllers/trades_controller.rb
@@ -1,40 +1,17 @@
 class TradesController < ApplicationController
   def trade
-    # Check for infected survivors
-    survivors.each do |survivor|
-      instance_variable_set("@#{survivor}", Survivor.find(trade_params[survivor][:id]))
-      render(json: { error: "#{survivor.to_s.humanize} is infected! Run away from him!!!" },
-             status: :conflict) && return if instance_variable_get("@#{survivor}").infected?
-      instance_variable_set("@#{survivor}_resources", [])
-    end
+    @survivor_1 = Survivor.find(trade_params[:survivor_1][:id])
+    @survivor_2 = Survivor.find(trade_params[:survivor_2][:id])
 
-    # Check for inconsistent resources
-    survivors.each do |survivor|
-      trade_params[survivor][:resources].each do |resource|
-        render(json: { error: "Invalid resources for #{survivor.to_s.humanize}" },
-               status: :unprocessable_entity) && return unless valid_resources?(survivor, resource)
-        instance_variable_set "@#{survivor}_resources",
-                              instance_variable_get("@#{survivor}_resources") +
-                              instance_variable_get("@#{survivor}")
-                              .resources.where(type: resource[:type])
-                              .first(resource[:amount].to_i)
-      end
-    end
+    trade = Trade.new(@survivor_1, @survivor_2,
+                      trade_params[:survivor_1][:resources],
+                      trade_params[:survivor_2][:resources])
+    trade.process
 
-    survivors.each do |survivor|
-      instance_variable_set "@#{survivor}_points",
-                            instance_variable_get("@#{survivor}_resources").map(&:points).inject(:+)
-    end
-
-    if @survivor_1_points == @survivor_2_points
-      Resource.where(id: @survivor_1_resources).update_all(survivor_id: @survivor_2.id)
-      Resource.where(id: @survivor_2_resources).update_all(survivor_id: @survivor_1.id)
-
-      render json: { message: 'Resources where traded sucessufuly' },
-             status: :ok
+    if trade.valid
+      render json: { message: trade.message }, status: :ok
     else
-      render json: { error: 'Invalid amount of points between both sides' },
-             status: :unprocessable_entity
+      render json: { error: trade.message }, status: trade.status
     end
   end
 
@@ -43,14 +20,5 @@ class TradesController < ApplicationController
   def trade_params
     params.require(:trade).permit(survivor_1: [:id, resources: [:type, :amount]],
                                   survivor_2: [:id, resources: [:type, :amount]])
-  end
-
-  def valid_resources?(survivor, resource)
-    instance_variable_get("@#{survivor}").resources.where(type: resource[:type])
-                                         .count >= resource[:amount].to_i
-  end
-
-  def survivors
-    [:survivor_1, :survivor_2]
   end
 end
